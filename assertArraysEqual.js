41ef4f96ebb79d4890f603cc35b88b85dd72e341
Zeinaaaa <zainabalbasuny8181@gmail.com>
2021-10-27 13:40:22 -0400

    new file

---
+++ assertArraysEqual.js
@@ -0,0 +1,30 @@
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] === arr2[i]) {
+      return true;
+    }else{
+      return false;
+    }
+  }
+}
+
+const AssertArraysEqual = function(arr1, arr2){
+  const isEqual = eqArrays(arr1, arr2);
+  if (!isEqual){
+   console.log( "These arrays are not similar ")
+  }else{
+    console.log( "Perfect, these arrays are similar")
+  }
+};
+
+// TEST CODEeqArrays([1, 2, 3], [1, 2, 3]) // => true
+AssertArraysEqual([1, 2, 3], [3, 2, 1])// => false
+
+AssertArraysEqual(["1", "2", "3"], ["1", "2", "3"]) // => true
+AssertArraysEqual(["1", "2", "3"], ["1", "2", 3])// => false
+
+AssertArraysEqual([1, 2, 3], [1, 2, 3]);
