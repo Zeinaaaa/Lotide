3e92bf43ffc8f7d3c435dce1cd5d53706c97c986
Zeinaaaa <zainabalbasuny8181@gmail.com>
2021-11-01 07:10:04 -0400

    refracroting tail.js

--- tail.js
+++ tail.js
@@ -10,13 +10,5 @@ const tail = (array) => {
   let b = array.slice(1);
   return b;
 };
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(50, "50");
-assertEqual(11, 1);
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+ module.exports = tail;
---
+++ test/tailTest.js
@@ -0,0 +1,6 @@
+const tail = require("../Tail.js");
+const assertEqual = require("../assertEqual.js")
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
